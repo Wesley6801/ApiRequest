eeb0957d632786c0c5392d51d60f32419df6318a
Wesley White <wesleywhite424@hotmail.com>
2021-06-30 19:27:36 +0000

    Ability to add API data to Database

--- spotify.py
+++ spotify.py
@@ -1,5 +1,8 @@
 import spotipy
 import requests
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
 
 client_id = 'bb7e8950c9834eaea96062eb752e5ddf'
 client_secret = '4a37f3ed348644959f6f1594fb24e915'
@@ -11,13 +14,32 @@ auth_response = requests.post(auth_url,{
 })
 
 auth_response_data = auth_response.json()
+
 access_token = auth_response_data['access_token']
+
 headers = {
   'Authorization' : 'Bearer {token}'.format(token=access_token)
 }
 BASE_URL = 'https://api.spotify.com/v1/'
 drake_uri = '3TVXtAsR1Inumwj472S9r4'
-r = requests.get(BASE_URL + 'artists/' + drake_uri + '/albums', headers=headers,)
-r2 = r.json()
-for key, value in r2.items():
-    print(key, " : ", value)
+
+# Test 1 : Make sure response returns 200
+# Test 2 : Make sure I get the album name and release_date
+response = requests.get(BASE_URL + 'artists/' + drake_uri + '/albums', headers=headers,)
+json_response = response.json()
+
+names = []
+release_dates = []
+# Test 1 : Make sure the first 10 albums and release_dates are shown
+for i in range(0,10):
+  names.append(json_response['items'][i]['name'])
+  release_dates.append(json_response['items'][i]['release_date'])
+data = {
+  'Album Name' : names,
+  'Release_Date' : release_dates     
+}
+col_names = ['Album Name', 'Release_Date']
+df = pd.DataFrame(data = data)
+
+engine = create_engine('mysql://root:codio@localhost/album_info')
+df.to_sql('albums', con=engine, if_exists='replace', index=False)
